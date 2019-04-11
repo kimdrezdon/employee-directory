ca344f9ea4d875146cef362b9f2ced8b58c1b681
Kim Drezdon <kdrezdon@gmail.com>
2019-04-10 21:17:21 -0500

    Adds createModal function

--- js/script.js
+++ js/script.js
@@ -44,48 +44,61 @@ function addButton(className, parent, id, innerHTML) {
 
 $('#gallery').on('click', '.card', function() {
     const i = $('.card').index(this);
-    const employee = employees[i];
-    
-    const modalContainer = addElement('div', 'modal-container', document.querySelector('body'));
-
-    const modalDiv = addElement('div', 'modal', modalContainer);
-
-    const closeButton = addButton('modal-close-btn', modalDiv, 'modal-close-btn', '<strong>X</strong>');
-
-    const modalInfo = addElement('div', 'modal-info-container', modalDiv);
-  
-    const modalImg = addElement('img', 'modal-img', modalInfo);
-    modalImg.setAttribute('src', employees[i].picture.large);
-    modalImg.setAttribute('alt', 'profile picture');
-
-    const name = addElement('h3', 'modal-name cap', modalInfo);
-    name.setAttribute('id', 'name');
-    name.textContent = `${employees[i].name.first} ${employees[i].name.last}`;
-    
-    const email = addElement('p', 'modal-text', modalInfo);
-    email.textContent = employees[i].email;
-
-    const location = addElement('p', 'modal-text cap', modalInfo);
-    location.textContent = employees[i].location.city;
-
-    modalInfo.appendChild(document.createElement('hr'));
-
-    const phone = addElement('p', 'modal-text', modalInfo);
-    phone.textContent = employees[i].cell;
-
-    const address = addElement('p', 'modal-text cap', modalInfo);
-    address.textContent = `${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}`;
-
-    const birthday = addElement('p', 'modal-text', modalInfo);
-    birthday.textContent = `Birthday: ${employee.dob.date.slice(5,7)}/${employee.dob.date.slice(8,10)}/${employee.dob.date.slice(0,4)}`;
-
-    const modalButtonDiv = addElement('div', 'modal-btn-container', modalContainer);
-
-    const prevButton = addButton('modal-prev btn', modalButtonDiv, 'modal-prev', 'Prev');
-
-    const nextButton = addButton('modal-next btn', modalButtonDiv, 'modal-next', 'Next');
+    createModal(i);
 })
+
+function createModal (i) {
+  const employee = employees[i];
+    
+  const modalContainer = addElement('div', 'modal-container', document.querySelector('body'));
+
+  const modalDiv = addElement('div', 'modal', modalContainer);
+
+  const closeButton = addButton('modal-close-btn', modalDiv, 'modal-close-btn', '<strong>X</strong>');
+
+  const modalInfo = addElement('div', 'modal-info-container', modalDiv);
+
+  const modalImg = addElement('img', 'modal-img', modalInfo);
+  modalImg.setAttribute('src', employees[i].picture.large);
+  modalImg.setAttribute('alt', 'profile picture');
+
+  const name = addElement('h3', 'modal-name cap', modalInfo);
+  name.setAttribute('id', 'name');
+  name.textContent = `${employees[i].name.first} ${employees[i].name.last}`;
+  
+  const email = addElement('p', 'modal-text', modalInfo);
+  email.textContent = employees[i].email;
+
+  const location = addElement('p', 'modal-text cap', modalInfo);
+  location.textContent = employees[i].location.city;
+
+  modalInfo.appendChild(document.createElement('hr'));
+
+  const phone = addElement('p', 'modal-text', modalInfo);
+  phone.textContent = employees[i].cell;
+
+  const address = addElement('p', 'modal-text cap', modalInfo);
+  address.textContent = `${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}`;
+
+  const birthday = addElement('p', 'modal-text', modalInfo);
+  birthday.textContent = `Birthday: ${employee.dob.date.slice(5,7)}/${employee.dob.date.slice(8,10)}/${employee.dob.date.slice(0,4)}`;
+
+  const modalButtonDiv = addElement('div', 'modal-btn-container', modalContainer);
+
+  const prevButton = addButton('modal-prev btn', modalButtonDiv, 'modal-prev', 'Prev');
+
+  const nextButton = addButton('modal-next btn', modalButtonDiv, 'modal-next', 'Next');
+}
 
 $('body').on('click', '#modal-close-btn', function() {
   document.querySelector('body').removeChild(document.querySelector('.modal-container'));
+});
+
+$('body').on('click', '#modal-prev', function() {
+  console.log('prev button clicked');
+  //createModal(i-1);
+});
+
+$('body').on('click', '#modal-next', function() {
+  console.log('next button clicked');
 });
